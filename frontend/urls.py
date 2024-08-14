e9586ee0017ebc6285f9b4ea0299ea3d28a1f34f
tonypal-2002 <tonypal2002@gmail.com>
2024-08-14 18:55:38 +0530

    frontend urls update

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from frontend.views import forget_password, home
 
 urlpatterns = [
-    path('' , home),
+    path('' , home, name='home'),
     path('forget_password', forget_password),
 ]
