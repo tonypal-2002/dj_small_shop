c6f35448e9403ea5bb482dfe2b24669bd0625b3d
tonypal-2002 <tonypal2002@gmail.com>
2024-08-16 11:07:50 +0530

    invoice logic added

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from config import settings
+
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls'))
-]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    path('', include('backend.urls')),
+]+static (settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
