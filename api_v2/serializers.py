439fcc4c2de9fdb7b8dab454c0af1fe6afcbf7bd
tonypal-2002 <tonypal2002@gmail.com>
2024-08-16 11:57:08 +0530

    brand api logic added

--- api_v2/serializers.py
+++ api_v2/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from backend.models import Category
+from backend.models import Category, Brand
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
--- api_v2/views.py
+++ api_v2/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 
-from backend.models import Category
+from backend.models import Category, Brand
 
-from api_v2.serializers import CategorySerializer
+from api_v2.serializers import CategorySerializer, BrandSerializer
+
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category. objects.all()
@@ -12,6 +13,17 @@ class CategoryListView(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
+        data = {
+            "data" : serializer.data
+        }
+        return Response(data)
+
+class BrandListView(generics.ListAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many= True)
         data = {
             "data" : serializer.data
         }
