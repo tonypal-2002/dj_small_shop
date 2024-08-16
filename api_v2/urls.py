a7b0357a3a8c797263a6a0f01e0838955ba777b6
tonypal-2002 <tonypal2002@gmail.com>
2024-08-16 18:49:59 +0530

    Update urls.py

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from api_v2.views import CategoryListView, BrandListView, ProductListView, OrderListView, CartView, CartItemView, \
-    ClearCartView
+    ClearCartView, CustomAuthToken, UserCreateAPIView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
