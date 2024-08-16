9f288b6204c4caa32083db99d9e5eea1f3c09abc
tonypal-2002 <tonypal2002@gmail.com>
2024-08-16 11:54:40 +0530

    category api logic added

--- api_v2/views.py
+++ api_v2/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from backend.models import Category
+
+from api_v2.serializers import CategorySerializer
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category. objects.all()
+    serializer_class =  CategorySerializer
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            "data" : serializer.data
+        }
+        return Response(data)
