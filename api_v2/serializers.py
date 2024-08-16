154e6f0e1fcf87a043403e323a813c86d1a27d55
tonypal-2002 <tonypal2002@gmail.com>
2024-08-16 18:45:08 +0530

    User Login Logic added to api v2

--- api_v2/serializers.py
+++ api_v2/serializers.py
@@ -1,8 +1,48 @@
 from rest_framework import serializers
 
-from backend.models import Category, Brand, Product, Order, Cart, OrderItem
+from backend.models import Category, Brand, Product, Order, Cart, OrderItem, CustomUser
 
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import authenticate
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = [
+            'gender',
+            'password',
+            'email'
+        ]
+    def create(self, validated_data):
+        user = super(CustomUserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        # group = Group.objects.get(name='EMPLOYEE')
+        # user.groups.add(group)
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+        return user
+
+class EmailAuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(label=_("Email"))
+    password = serializers.CharField(label=_("Password"), style={'input_type': 'password'}, trim_whitespace=False)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'), username=email, password=password)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
--- api_v2/views.py
+++ api_v2/views.py
@@ -1,14 +1,51 @@
 import self
 from rest_framework import generics, status
-from rest_framework.generics import get_object_or_404
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import get_object_or_404, CreateAPIView
 from rest_framework.response import Response
 
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 
-from backend.models import Category, Brand, Product, Order, Cart
+from backend.models import Category, Brand, Product, Order, Cart, CustomUser
 
-from api_v2.serializers import CategorySerializer, BrandSerializer, ProductSerializer, OrderSerializer, CartSerializer
+from api_v2.serializers import CategorySerializer, BrandSerializer, ProductSerializer, OrderSerializer, CartSerializer, \
+    EmailAuthTokenSerializer, CustomUserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({"message": "User successfully registered"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomAuthToken(ObtainAuthToken):
+
+    serializer_class = EmailAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,context={'request':request})
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token,created = Token.objects.get_or_create(user=user)
+        # return Response({
+        #     'token_type':'token',
+        #     'token':token.key,
+        #     'user_id':user.pk,
+        #     'email':user.email
+        # })
+
+
+        return  Response(token.key)
 
 
 class CategoryListView(generics.ListAPIView):
