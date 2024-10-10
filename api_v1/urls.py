ca94ed2b80ea1251be77c3fb9c3c7ac709b0446a
tonypal-2002 <tonypal2002@gmail.com>
2024-10-10 17:06:44 +0530

    api_v1 urls.py added

---
+++ api_v1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('categories/', views.CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', views.CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
+]
